da4246524e727ebfd6ace800dabc30f3dbf8fc59
ManiMazaheri1385 <manimazaheri.13852006@gmail.com>
2025-04-11 14:35:25 +0330

    modify light class

--- src/devices/Light.java
+++ src/devices/Light.java
@@ -3,10 +3,9 @@ package devices;
 import db.Device;
 import db.Validator;
 
-public class Light extends Device implements Validator {
+public class Light extends Device {
 
     public int brightness;
-    public String name;
 
     public Light(String name, Protocol protocol) {
         super(name, protocol);
@@ -14,19 +13,16 @@ public class Light extends Device implements Validator {
     }
 
     @Override
-    public void setValue(int brightness) {
-        if (brightness < 0 || brightness > 100) {
-            throw new IllegalArgumentException("brightness must be between 0 and 100");
-        }
-        this.brightness = brightness;
+    public String information() {
+        return "light: " + name + " " + status + " " + brightness + "% " + protocol;
     }
 
     @Override
-    public void setName(String name) {
-        if (name == null || name.isEmpty()) {
-            throw new IllegalArgumentException("Name cannot be null or empty");
+    public void setValue(int brightness) {
+        if (brightness < 0 || brightness > 100) {
+            throw new IllegalArgumentException("invalid value");
         }
-        this.name = name;
+        this.brightness = brightness;
     }
 
 }
