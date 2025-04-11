b116d4503bec388db5bdcbe83e7b4598255a8ef2
ManiMazaheri1385 <manimazaheri.13852006@gmail.com>
2025-04-11 18:58:31 +0330

    fix exception in light service class

--- src/devices/services/LightService.java
+++ src/devices/services/LightService.java
@@ -1,7 +1,7 @@
 package devices.services;
 
-import db.Database;
 import db.Device;
+import db.Database;
 import devices.Light;
 
 public class LightService {
@@ -9,22 +9,20 @@ public class LightService {
     private LightService() {}
 
     public static void addLight(String name, String protocolString) {
-        Device.Protocol protocol = Device.Protocol.valueOf(protocolString);
-        Light light = new Light(name, protocol);
-        Database.add(light);
+        try {
+            Device.Protocol protocol = Device.Protocol.valueOf(protocolString);
+            Light light = new Light(name, protocol);
+            Database.addDevice(light);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("invalid protocol");
+        }
     }
 
     public static void setBrightness(Device device, String brightnessString) {
         Light light = (Light) device;
         int brightness = Integer.parseInt(brightnessString);
         light.setValue(brightness);
-        try {
-            Database.update(light);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        Database.update(light);
     }
 
-    public static void removeLight(String name) {}
 }
